b2d14c9df5a70e37a05fe1da39aae6cef103bbaa
Ahmad Faiaz <ahmadfaiaz22@gmail.com>
2026-06-07 13:23:45 -0400

    starting part four - module part 1

---
+++ Python-tutorial/my_module.py
@@ -0,0 +1,10 @@
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+# module can contain executable statment and func definition. statements are executed 
+# only the first time the module name is encountered in an import statement
+print(__name__)
