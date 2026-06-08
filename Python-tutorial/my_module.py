eb876ef5dd3aad1a46efd22c6b21fcad92bb7185
Ahmad Faiaz <ahmadfaiaz22@gmail.com>
2026-06-07 23:56:39 -0400

    part-4 - module part-2

--- Python-tutorial/my_module.py
+++ Python-tutorial/my_module.py
@@ -7,4 +7,9 @@ def sub(a, b):
 
 # module can contain executable statment and func definition. statements are executed 
 # only the first time the module name is encountered in an import statement
-print(__name__)
+
+print("hey I am out side the main module lol")
+
+if __name__ == '__main__':
+    print(__name__)
+    print(__spec__)
